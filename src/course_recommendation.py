c64ce606c417072ceebb923d585ecc976c22f3ca
Harshini Kovur <hvkovur@ncsu.edu>
2024-12-13 23:34:35 -0500

    fixed errors

--- src/course_recommendation.py
+++ src/course_recommendation.py
@@ -3,6 +3,6 @@ def recommend_courses(user_skills, available_courses):
     for course in available_courses:
         overlap = set(user_skills).intersection(set(course['skills_required']))
         if overlap:
-            recommendations.append({'course': course['name'], 'match_score': len(overlap)})
-    return sorted(recommendations, key = lambda x: x['match_score'], reverse = True)
+            reommendations.append({'course': course['name'], 'match_score': len(overlap)})
+    return sorted(reommendations, key = lambda x: x['match_score'], reverse = True)
     
